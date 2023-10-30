813756df492c85039a2de64c2f66b8d0f95f2b8b
vijay-aruchamy <vijayaruchamy2003@gmail.com>
2023-10-30 18:07:29 +0530

    field getting commit

--- Practice/Problems/ConvertJsonToJavaProblem/ConvertJsonToJava.java
+++ Practice/Problems/ConvertJsonToJavaProblem/ConvertJsonToJava.java
@@ -42,12 +42,29 @@ public class ConvertJsonToJava {
         // String json2 = "{\"name\": \"John Doe\", \"age\": 30, \"phoneNo\":\"0923456664\"}";
         Map<String, Object> data = convertJsonToMap(json);
         ConvertJsonToJava obj1 = new ConvertJsonToJava(data);
-        // System.out.println(obj1.get);
+
+        
+         Field[] fields = ConvertJsonToJava.getDeclaredFields();
+
+         
+         Map<String, Class<?>> fieldMap = new HashMap<>();
+ 
+       
+         for (Field field : fields) {
+             String fieldName = field.getName();
+             Class<?> fieldType = field.getType();
+             fieldMap.put(fieldName, fieldType);
+         }
+       
         Class<?> A=obj1.getClass();
         Field fieldss[] = A.getDeclaredFields();
         for(int i=0;i<fieldss.length;i++)
         System.out.println(fieldss[i]);
 
         System.out.println(obj1.name + " " + obj1.age + " " + obj1.phoneNo);
+    }
+
+    private static Field[] getDeclaredFields() {
+        return null;
     }
 }
---
+++ Practice/Problems/ConvertJsonToJavaProblem/JSONException.java
@@ -0,0 +1,5 @@
+package Practice.Problems.ConvertJsonToJavaProblem;
+
+public class JSONException {
+
+}
